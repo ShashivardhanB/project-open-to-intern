3d73a2fe063733a3a604b2f810ee9d67e6d2eefa
ShashivardhanB <itsmeshashivardhanreddy8340@gmail.com>
2022-03-24 08:33:10 +0530

    updated code

--- src/controllers/internController.js
+++ src/controllers/internController.js
@@ -3,14 +3,9 @@ const internModel = require("../models/internModel")
 
 
 const isValid = function (value) {
-  if (typeof (value) === 'undefined' || typeof (value) === 'null') {
-    return false
-  }
-  if (value.trim().length == 0) {
-    return false
-  } if (typeof (value) === 'string' && value.trim().length > 0) {
-    return true
-  }
+  if (typeof value === 'undefined' || value === null) return false
+  if (typeof value === 'string' && value.trim().length === 0) return false
+  return true;
 }
 
 const isValidRequestBody = function (requestBody) {
